02e5ad6cc7d6ea178ca7d839e6fb41a127113e5c
chtmorris <agoosester@gmail.com>
2016-09-16 12:20:21 +0800

    Added receive ether VC

---
+++ EVA Wallet/EVARecieveEtherVC.swift
@@ -0,0 +1,28 @@
+//
+//  EVARecieveEtherVC.swift
+//  EVA Wallet
+//
+//  Created by Charlie Morris on 9/16/16.
+//  Copyright © 2016 Flailing Whale. All rights reserved.
+//
+
+import UIKit
+
+class EVARecieveEtherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func downButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
